c66b041ab4c594e548c7f1cde375031b72e86a31
Raptor7742 <106883599+Raptor7742@users.noreply.github.com>
2023-01-01 22:35:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const playerInstance = jwplayer("player").setup({
 
   playlist: [
     {
-      title: "Arrête de me chauffer, Nagatoro  - épisode 1 VOSTFR",
+      title: "Arrête de me chauffer, Nagatoro  - épisode 5 VOSTFR",
       description: "Vous regardez",
       image: "https://cdn.statically.io/gh/Anime-Sama/IMG/img/animes/animes%20wallpapers/ijiranaide-nagatorocarousel.jpg",
       sources: [
@@ -37,7 +37,7 @@ const playerInstance = jwplayer("player").setup({
         },
         {
           file:
-            "",
+            "https://m210.syncusercontent.com/mfs-60:51d2d37e8d45735e9f2357a735e00822=============================/p/épisode%205.mp4?allowdd=0&datakey=sP/FGmaN79u6Xp8XC9Xxij4XxGDZ6ioICG3Xjd8ilHDz+KpviGTU1F3ElobwfWZrGmd17MFRZQw2p/Yswkxi2cU7SJupeBhrW3hLh0MSffsm4V4jLg/9mn+EypU2emMeCfrVNFEmOxH9kKrZqGPUvvh2hjGut14eHiRt9jM87usdMvxOf/YNneQlceJORrwl76tJPZhcGVrnKp8HMSH8aJr12J1nZ6QK8KlvdXaW1wnARwwn1YqKu/BNUVqGNnQ3Rgvly1KuJrLvKdCvpHlToAI8/T9AIguOLilxAM6ejv2QRJ3L3VRpYPXoW/NmwqUraijsS3bs1w6HrT97xYXKgQ&engine=ln-2.3.7.3&errurl=jk35QlRv25uClQmG5ArtZIO+LspeOZ/Cok9K6FDL3+GbbwnWNm56mzyBFfAKzKBzqncX7fJeUBFwWgqmSSGts8krpJmNw6VId94RtynSlcomOADlF/SduvOJuaRx6R5ZD6+Ei7PXs8Dd54SWw/EAaVOwGkMUMBbijtLUHntlKfFWJvMaGOUJ29wuYjuz/bKUZwF5b2pVlI9Nxg+OS/NbtWybKzPns6CDJMEkle67kVOAakdjvNnwH3/0aHJFcldkMaQqbTvmywgczXHxaj+jAb0oKe3K8lrzqgnsBqoziykU9kvCsu+PAvgbB/pU0T4eYcaE8lOC5PbFaLWN/n4fyg==&header1=Q29udGVudC1UeXBlOiB2aWRlby9tcDQ&header2=Q29udGVudC1EaXNwb3NpdGlvbjogaW5saW5lOyBmaWxlbmFtZT0iJUMzJUE5cGlzb2RlJTIwNS5tcDQiO2ZpbGVuYW1lKj1VVEYtOCcnJUMzJUE5cGlzb2RlJTIwNS5tcDQ7&ipaddress=1458994159&linkcachekey=f800b2560&linkoid=652380000&mode=101&sharelink_id=8937747390000&timestamp=1672608904533&uagent=220523ca5285197b0fad467e0e72e6907a6c5738&signature=ad03d0b3dec411d05a22a231fda073e93ba40e15&cachekey=60:51d2d37e8d45735e9f2357a735e00822=============================",
           label: "720p"
         },
         {
